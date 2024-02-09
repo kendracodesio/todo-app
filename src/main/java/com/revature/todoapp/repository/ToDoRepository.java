b9ab9834e068d1fdb502153d790ba787dbdc5985
Kendra <kendracodesio@gmail.com>
2024-02-08 16:44:02 -0800

    adds code to repository interfaces

--- src/main/java/com/revature/todoapp/repository/ToDoRepository.java
+++ src/main/java/com/revature/todoapp/repository/ToDoRepository.java
@@ -1,4 +1,18 @@
 package com.revature.todoapp.repository;
 
-public interface ToDoRepository {
+import com.revature.todoapp.entity.Category;
+import com.revature.todoapp.entity.ToDo;
+import com.revature.todoapp.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface ToDoRepository extends JpaRepository<ToDo, Integer> {
+
+    List<ToDo> findAllByCreatedBy(User createdBy);
+    List<ToDo> findAllByCreatedByAndIsComplete(User createdBy, boolean isComplete);
+    List<ToDo> findAllByCreatedByAndCategory(User createdBy, Category category);
+
+
 }
--- src/main/java/com/revature/todoapp/repository/UserRepository.java
+++ src/main/java/com/revature/todoapp/repository/UserRepository.java
@@ -1,4 +1,13 @@
 package com.revature.todoapp.repository;
 
-public interface UserRepository {
+import com.revature.todoapp.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByUsernameAndPassword(String username, String password);
 }
